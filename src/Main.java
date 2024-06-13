8846c6db593fee9f53f2e52b36dffed986206aa5
Alex1445085 <n1445085@yandex.ru>
2024-06-13 23:00:48 +0300

    tasks 1-7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+// 1.
         System.out.println("1.");
         int deposit = 0, month = 0;
         while (deposit <= 2459000) {
@@ -10,6 +11,7 @@ public class Main {
         }
         System.out.println();
 
+// 2.
         System.out.println("2.");
         int i = 1;
         while (i <= 10) {
@@ -21,6 +23,7 @@ public class Main {
             System.out.print(i + " ");
         }
 
+// 3.
         System.out.println("\n");
         System.out.println("3.");
         int populationY = 12000000;
@@ -33,6 +36,8 @@ public class Main {
             System.out.printf("Год %d, численность населения составляет %d\n", i, populationY);
         }
 
+
+// 4.
         System.out.println();
         System.out.println("4.");
         double depositInBank = 15000;
@@ -43,6 +48,7 @@ public class Main {
             System.out.printf("После %d месяца вклад вырос до %.2f руб\n", i, depositInBank);
         }
 
+// 5.
         System.out.println();
         System.out.println("5.");
         depositInBank = 15000;
@@ -55,6 +61,34 @@ public class Main {
             }
         }
         System.out.printf("После %d месяца вклад наконец то превысил 12 000 000 и составил %.2f р\n", i, depositInBank);
+
+// 6.
+        System.out.println();
+        System.out.println("6.");
+        depositInBank = 15000;
+        int monthPercent = 7;
+        int depositYear = 12 * 9;
+        for (i = 1; i <= depositYear; i++) {
+            depositInBank = depositInBank + depositInBank * monthPercent / 100;
+            if (i % 6 == 0) {
+                System.out.printf("После %d месяца накопления составляют %.2fр.\n", i, depositInBank);
+            }
+        }
+
+// 7.
+        System.out.println("\n7.");
+        int monthFirstFriday = 2;
+        for (i = monthFirstFriday; i <= 31; i = i + 7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+        // v2
+        System.out.println();
+        for (i = monthFirstFriday; i <= 31; i++) {
+            if ((i - monthFirstFriday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+            }
+
+        }
 
     }
 }
