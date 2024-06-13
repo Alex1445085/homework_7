2967c3dced8011577b74322cbb4af71f2ff8f048
Alex1445085 <n1445085@yandex.ru>
2024-06-13 21:13:19 +0300

    task 1,2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
             month++;
             System.out.printf("Месяц %d, сумма накоплений равна %d\n", month, deposit);
         }
+        System.out.println();
+
+        System.out.println("2.");
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
     }
 }
