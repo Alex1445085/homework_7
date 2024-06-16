9e555b9731744d64acaa37ff909656472fccc14a
Alex1445085 <n1445085@yandex.ru>
2024-06-16 10:12:18 +0300

    tasks 1-8. 1st task modified

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         while (deposit <= purposeForDeposit) {
             deposit += contribution;
             month++;
-            System.out.printf("Месяц %d, сумма накоплений равна %d\n", month, deposit);
+            System.out.printf("Месяц %d, сумма накоплений равна %d\n рулей", month, deposit);
         }
         System.out.println();
 
@@ -104,6 +104,7 @@ public class Main {
             }
         }
     }
+
 
     private static double percentage(double depositInBank, int monthPercent) {
         return depositInBank * (100 + monthPercent) / 100;
